71d7bd3b19bb95ea1504415e49b03425d3d34f32
Abhi088 <abhicoding8@gmail.com>
2021-11-29 01:46:31 +0530

    Deploy

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== "production") {
+     require('dotenv').config();
+ } 
+
 import express, { Router }  from "express";
 import Connection from "./database/db.js";
 import router from "./Routes/Route.js";
@@ -11,9 +15,10 @@ app.use(bodyParser.json({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
 //package-lock-jsn acts as a information provider for package-json depndencies
 //for creating the api  we have to use the module router
-app.use('/',router)
-
-
+app.use('/',router);
+app.get("/", (req, res) => {
+     res.status(200).json("BLOG APPLICATION");
+})
 
 const PORT = process.env.PORT | 8000;
 app.listen(PORT,  ()=>{
