e9bc1f2590581e70ead5583740e864b49efdf674
Abhi088 <abhicoding8@gmail.com>
2021-11-29 01:51:42 +0530

    deploy

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ app.get("/", (req, res) => {
 })
 
 const PORT = process.env.PORT | 8000;
-app.listen(PORT,  ()=>{
-     
+app.listen(PORT,  ()=>{   
      console.log(`server is running successfully on port ${PORT}`)
 })
 
